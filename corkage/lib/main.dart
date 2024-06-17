db140544edd381f4434e1da7548b73a506a2137b
haa741123 <ihamin012@gmail.com>
2024-06-17 18:44:00 +0900

    네비게이션 바 생성

--- corkage/lib/main.dart
+++ corkage/lib/main.dart
@@ -17,21 +17,20 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter WebView Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0; // 선택된 탭 인덱스
 
   @override
   void initState() {
@@ -49,23 +48,72 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  @override
-  void dispose() {
-    super.dispose();
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index; // 선택된 인덱스 업데이트
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(  // SafeArea 위젯으로 감싸서 상태바에 겹치지 않도록 함
-        child: InAppWebView(
-          initialUrlRequest: URLRequest(
-            url: WebUri('http://121.142.17.86/index'),
-          ),
-          initialOptions: InAppWebViewGroupOptions(
-            android: AndroidInAppWebViewOptions(useHybridComposition: true),
-          ),
+      body: SafeArea(
+        child: IndexedStack(
+          index: _selectedIndex,
+          children: <Widget>[
+            InAppWebView(
+              initialUrlRequest: URLRequest(
+                url: WebUri('http://121.142.17.86/index'),
+              ),
+              initialOptions: InAppWebViewGroupOptions(
+                android: AndroidInAppWebViewOptions(useHybridComposition: true),
+              ),
+            ),
+            Container(
+              color: Colors.green,
+              child: const Center(
+                child: Text('라벨 인식 화면', style: TextStyle(fontSize: 24, color: Colors.white)),
+              ),
+            ),
+            Container(
+              color: Colors.blue,
+              child: const Center(
+                child: Text('커뮤니티 화면', style: TextStyle(fontSize: 24, color: Colors.white)),
+              ),
+            ),
+            Container(
+              color: Colors.orange,
+              child: const Center(
+                child: Text('마이페이지 화면', style: TextStyle(fontSize: 24, color: Colors.white)),
+              ),
+            ),
+          ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.photo_camera),
+            label: '라벨 인식',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: '커뮤니티',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: '마이페이지',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.deepPurple, // 선택된 아이템 색상
+        unselectedItemColor: Colors.grey, // 선택되지 않은 아이템 색상 설정
+        type: BottomNavigationBarType.fixed, // 모든 아이템의 크기와 색상 고정
+        onTap: _onItemTapped,
       ),
     );
   }
